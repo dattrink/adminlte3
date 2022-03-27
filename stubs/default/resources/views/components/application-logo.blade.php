255b67154f55365085c203a6963771b199e8e22f
ttdat <ttdat.email@gmail.com>
2022-03-27 17:43:42 +0700

    Fix home route name

--- stubs/default/resources/views/components/application-logo.blade.php
+++ stubs/default/resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('dashboard') }}" class="brand-link">
+<a href="{{ route('home') }}" class="brand-link">
   <img src="{{ asset('storage/images/adminlte3/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
   <span class="brand-text font-weight-light">AdminLTE 3</span>
 </a>
